41f8f88fa2c4e43fe615172ec6999ea9f93e34ee
Muhammad Fazle Rabbani <f4zlerabbani@gmail.com>
2026-04-14 11:31:20 +0700

    Update DetailViewModel to include errorLD and implement onCleared

--- app/src/main/java/com/example/studentproject/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/studentproject/viewmodel/DetailViewModel.kt
@@ -14,11 +14,13 @@ import com.google.gson.reflect.TypeToken
 
 class DetailViewModel(application: Application): AndroidViewModel(application) {
     val studentLD = MutableLiveData<Student>()
+    val errorLD = MutableLiveData<Boolean>()
     private var TAG = "volley_tag"
     private var queue: RequestQueue?= null
 
     fun fetch(id: String) {
         val url = "https://www.jsonkeeper.com/b/LLMW"
+        errorLD.value = false
 
         var stringRequest = StringRequest(
             Request.Method.GET, url,
@@ -31,11 +33,17 @@ class DetailViewModel(application: Application): AndroidViewModel(application) {
             },
             {
                 Log.e("volley_status", it.message.toString())
+                errorLD.value = true
             })
         stringRequest.tag = TAG
         queue?.add(stringRequest)
 
         // val student1 = Student("16055","Nonie","1998/03/28","5718444778", "http://dummyimage.com/75x100.jpg/cc0000/ffffff")
         // studentLD.value = student1
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        queue?.cancelAll(TAG)
     }
 }
